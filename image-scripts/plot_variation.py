cb5068e9c847ff02ae93f35af8a6c5b4d863d655
Matthew Rocklin <mrocklin@gmail.com>
2013-06-12 08:46:13 -0500

    whitespace

--- image-scripts/plot_variation.py
+++ image-scripts/plot_variation.py
@@ -5,12 +5,12 @@ times_send = loadtxt('image-scripts/data/times_send.dat')
 times_recv = loadtxt('image-scripts/data/times_recv.dat')
 groups = times_send.reshape(100, 100)
 ssizes = sizes.reshape(100, 100).mean(1)
+
 figure()
 xlabel('Bytes')
 ylabel('Relative Variation (unitless)')
 loglog(ssizes*8, groups.std(1) / groups.mean(1))
 savefig('images/communication-variation.pdf')
-
 
 figure()
 xlabel('Bytes')
