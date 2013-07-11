9941774e29cba62248bfe4d095c2c968e3a97b85
Matthew Rocklin <mrocklin@gmail.com>
2013-07-11 15:03:49 -0500

    align kalman

--- kalman.py
+++ kalman.py
@@ -1,15 +1,17 @@
 from sympy import Symbol, MatrixSymbol, latex
 
-n       = Symbol('n')                   # Number of variables in our system/current state
-k       = Symbol('k')                   # Number of variables in the observation
-mu      = MatrixSymbol('mu', n, 1)      # Mean of current state
+n       = Symbol('n')                   # Number of variables in state
+k       = Symbol('k')                   # Number of variables in observation
+mu      = MatrixSymbol('mu',    n, 1)   # Mean of current state
 Sigma   = MatrixSymbol('Sigma', n, n)   # Covariance of current state
-H       = MatrixSymbol('H', k, n)       # A measurement operator on current state
-R       = MatrixSymbol('R', k, k)       # Covariance of measurement noise
-data    = MatrixSymbol('data', k, 1)    # Observed measurement data
+H       = MatrixSymbol('H',     k, n)   # Measurement operator
+R       = MatrixSymbol('R',     k, k)   # Covariance of measurement noise
+data    = MatrixSymbol('data',  k, 1)   # Observed measurement data
 
-newmu   = mu + Sigma*H.T * (R + H*Sigma*H.T).I * (H*mu - data)      # Updated mean
-newSigma= Sigma - Sigma*H.T * (R + H*Sigma*H.T).I * H * Sigma       # Updated covariance
+# Updated mean
+newmu   = mu + Sigma*H.T * (R + H*Sigma*H.T).I * (H*mu - data)
+# Updated covariance
+newSigma= Sigma - Sigma*H.T * (R + H*Sigma*H.T).I * H * Sigma
 
 assumptions = (Q.positive_definite(Sigma), Q.symmetric(Sigma),
                Q.positive_definite(R), Q.symmetric(R), Q.fullrank(H))
