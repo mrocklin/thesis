b46b38e4059a9f70528ca2c13319f8fd81c54a4f
Matthew Rocklin <mrocklin@gmail.com>
2013-07-19 17:27:44 -0500

    plot_variation includes model

--- image-scripts/plot_variation.py
+++ image-scripts/plot_variation.py
@@ -5,6 +5,7 @@ times_send = loadtxt('image-scripts/data/times_send.dat')
 times_recv = loadtxt('image-scripts/data/times_recv.dat')
 groups = times_send.reshape(100, 100)
 ssizes = sizes.reshape(100, 100).mean(1)
+bytes = ssizes * 8
 
 figure()
 xlabel('Bytes')
@@ -12,8 +13,14 @@ ylabel('Relative Variation (unitless)')
 loglog(ssizes*8, groups.std(1) / groups.mean(1))
 savefig('images/communication-variation.pdf')
 
+invband, latency = polyfit(sizes*8, times_recv, 1, w=1./sizes)
+model = lambda bytes: latency + bytes*invband
+
 figure()
 xlabel('Bytes')
 ylabel('Communication Time (s)')
-loglog(sizes, times_recv, '.')
+loglog(sizes*8, times_recv, '.')
+loglog(sizes*8, model(sizes*8))
+legend(['Data', 'Model'])
+title('Latency: %.1e Bandwidth: %.1e'%(latency, 1./invband))
 savefig('images/communication-time.pdf')
